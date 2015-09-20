902133bf5551f8d397b5be249ca42c5b41763851
Dinar Garipov <garipov.dinar@gmail.com>
2015-09-20 03:53:39 +0300

    removed underscores according to PSR

--- src/Pinger.php
+++ src/Pinger.php
@@ -24,57 +24,57 @@ class Pinger
 
     public function pingYandex($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing('http://ping.blogs.yandex.ru', $xml);
+        return $this->sendPing('http://ping.blogs.yandex.ru', $xml);
     }
 
     public function pingGoogle($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing('http://blogsearch.google.com/ping/RPC2', $xml);
+        return $this->sendPing('http://blogsearch.google.com/ping/RPC2', $xml);
     }
 
     public function pingYahoo($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing('http://api.my.yahoo.com/RPC2', $xml);
+        return $this->sendPing('http://api.my.yahoo.com/RPC2', $xml);
     }
 
     public function pingFeedburner($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing('http://ping.feedburner.com', $xml);
+        return $this->sendPing('http://ping.feedburner.com', $xml);
     }
 
     public function pingWeblogs($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing('http://ping.weblogs.se/', $xml);
+        return $this->sendPing('http://ping.weblogs.se/', $xml);
     }
 
     public function ping($service_url, $title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
-        return $this->_sendPing($service_url, $xml);
+        return $this->sendPing($service_url, $xml);
     }
 
     public function pingAll($title, $url, $rss = null)
     {
-        $xml = $this->_getXml($title, $url, $rss);
+        $xml = $this->getXml($title, $url, $rss);
 
         foreach ($this->services as $service) {
-            $this->_sendPing($service, $xml);
+            $this->sendPing($service, $xml);
         }
         return true;
     }
 
-    private function _getXml($title, $url, $rss = null)
+    private function getXml($title, $url, $rss = null)
     {
         $data = [
             'title' => $title,
@@ -86,7 +86,7 @@ class Pinger
         return view('laravel-pinger::xml', $data)->render();
     }
 
-    private function _sendPing($url, $post)
+    private function sendPing($url, $post)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
